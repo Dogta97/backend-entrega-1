4b7faa849c96f57dde05522029608c5e6fdb31fe
Dogta97 <jperrota24@gmail.com>
2026-03-14 13:56:04 -0300

    Entrega N° 2

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
-import express from 'express'
-import productsRouter from './routes/products.router.js'
-import cartsRouter from './routes/carts.router.js'
+import express from "express"
+import handlebars from "express-handlebars"
+
+import viewsRouter from "./routes/views.router.js"
+import productsRouter from "./routes/products.router.js"
+import cartsRouter from "./routes/carts.router.js"
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/products', productsRouter)
-app.use('/api/carts', cartsRouter)
+
+app.engine("handlebars", handlebars.engine())
+app.set("view engine", "handlebars")
+app.set("views", "./src/views")
+
+
+app.use("/", viewsRouter)
+app.use("/api/products", productsRouter)
+app.use("/api/carts", cartsRouter)
 
 export default app
--- src/server.js
+++ src/server.js
@@ -1,7 +1,79 @@
 import app from './app.js'
+import { Server } from 'socket.io'
+import ProductManager from './managers/ProductManager.js'
 
 const PORT = 8080
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`)
+const httpServer = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`)
+})
+
+const io = new Server(httpServer)
+
+app.set('io', io)
+
+const manager = new ProductManager('./src/data/products.json')
+
+
+io.on("connection", (socket) => {
+
+    console.log("Cliente conectado")
+
+
+    manager.getProducts().then(products => {
+
+        console.log("PRODUCTOS:", products)
+
+        socket.emit("updateProducts", products)
+
+    })
+
+
+    socket.on("addProduct", (product) => {
+
+        console.log("LLEGO PRODUCTO:", product)
+
+        manager.addProduct({
+            title: product.title,
+            description: "desc",
+            code: Date.now().toString(),
+            price: product.price,
+            status: true,
+            stock: 10,
+            category: "general",
+            thumbnails: []
+        })
+        .then(() => manager.getProducts())
+        .then(products => {
+
+            console.log("NUEVA LISTA:", products)
+
+            io.emit("updateProducts", products)
+
+        })
+
+    })
+
+
+    // ✅ DELETE PRODUCTO
+
+    socket.on("deleteProduct", async (id) => {
+
+        console.log("ELIMINAR:", id)
+
+        await manager.deleteProduct(id)
+
+        const products = await manager.getProducts()
+
+        io.emit("updateProducts", products)
+
+    })
+
+
+    socket.on("disconnect", () => {
+
+        console.log("Cliente desconectado")
+
+    })
+
 })
